e14c36a3b6b092234c932f69fe4625aa50d33407
rajachaymae <rajafamilly633@gmail.com>
2023-10-14 15:13:30 -0700

    up

---
+++ list.h
@@ -0,0 +1,14 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#endif
